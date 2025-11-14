93bce7bdb340a9fca829ab1528aab375b4fdb606
ehu <ehu@158.227.124.16>
2025-11-14 13:16:49 +0100

    2025/11/14 - Klasean egindakoa

---
+++ RidesJSF/src/main/java/eredua/bean/ChoiceBean.java
@@ -0,0 +1,11 @@
+package eredua.bean;
+
+import java.io.Serializable;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+@Named("choice")
+@SessionScoped
+public class ChoiceBean implements Serializable{
+	
+}
